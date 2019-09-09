9c98bc2512e191eadcccc746b5dd04b350d30765
Arbitrary <qinuo@gmail.com>
2019-09-09 09:35:10 +0800

    ww

--- src/test/java/test/JavaTest.java
+++ src/test/java/test/JavaTest.java
@@ -6,6 +6,11 @@ import com.xuguiping.common.utils.StringUtil;
 
 public class JavaTest {
 	@Test
+	public void toHtml(){
+		//测试StringUtil工具类中toHtml()方法，传一段文本进去
+		StringUtil.toHtml("asdqwr");
+	}
+	@Test
 	public void isPhone(){
 		//试StringUtil工具类中的是否为中国地区手机号码isPhone()工具方法
 		boolean phone = StringUtil.isPhone("1651351");
